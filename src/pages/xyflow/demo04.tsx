8480cc21289d7c5ee02093b682d0a989391a2bd5
xu.pengfei <1508075252@qq.com>
2025-01-07 21:10:14 +0800

    a

--- src/pages/xyflow/demo04.tsx
+++ src/pages/xyflow/demo04.tsx
@@ -1,114 +1,55 @@
-import React, { useCallback, useMemo } from "react";
-import {
-  ReactFlow,
-  useReactFlow,
-  ReactFlowProvider,
-  Background,
-  BackgroundVariant,
-  type Node,
-  type NodeMouseHandler, Controls, MiniMap,
-} from '@xyflow/react';
+import React, { KeyboardEventHandler, useCallback, useState } from "react";
+import { Background, BackgroundVariant, Controls, MiniMap, type NodeMouseHandler, ReactFlow, useReactFlow, } from '@xyflow/react';
 
-import slides from './cube/slides';
-import {
-  Slide,
-  SLIDE_WIDTH,
-  SLIDE_HEIGHT,
-  SLIDE_PADDING,
-  type SlideData,
-} from './cube/Slide';
+import { slides, slidesToElements } from './cube/slides';
+import { Slide, type SlideData, } from './cube/Slide';
 
-
-const slidesToElements = () => {
-  const start = Object.keys(slides)[0];
-  const stack = [{ id: start, position: { x: 0, y: 0 } }];
-  const visited = new Set();
-  const nodes = [];
-  const edges = [];
-
-  while (stack.length) {
-    const { id, position } = stack.pop();
-    const slide = slides[id];
-    const node = {
-      id,
-      type: 'slide',
-      position,
-      data: slide,
-      draggable: false,
-    } satisfies Node<SlideData>;
-
-    if (slide.left && !visited.has(slide.left)) {
-      const nextPosition = {
-        x: position.x - (SLIDE_WIDTH + SLIDE_PADDING),
-        y: position.y,
-      };
-
-      stack.push({ id: slide.left, position: nextPosition });
-      edges.push({
-        id: `${id}->${slide.left}`,
-        source: id,
-        target: slide.left,
-      });
-    }
-
-    if (slide.up && !visited.has(slide.up)) {
-      const nextPosition = {
-        x: position.x,
-        y: position.y - (SLIDE_HEIGHT + SLIDE_PADDING),
-      };
-
-      stack.push({ id: slide.up, position: nextPosition });
-      edges.push({ id: `${id}->${slide.up}`, source: id, target: slide.up });
-    }
-
-    if (slide.down && !visited.has(slide.down)) {
-      const nextPosition = {
-        x: position.x,
-        y: position.y + (SLIDE_HEIGHT + SLIDE_PADDING),
-      };
-
-      stack.push({ id: slide.down, position: nextPosition });
-      edges.push({
-        id: `${id}->${slide.down}`,
-        source: id,
-        target: slide.down,
-      });
-    }
-
-    if (slide.right && !visited.has(slide.right)) {
-      const nextPosition = {
-        x: position.x + (SLIDE_WIDTH + SLIDE_PADDING),
-        y: position.y,
-      };
-
-      stack.push({ id: slide.right, position: nextPosition });
-      edges.push({
-        id: `${id}->${slide.down}`,
-        source: id,
-        target: slide.down,
-      });
-    }
-
-    nodes.push(node);
-    visited.add(id);
-  }
-
-  return { start, nodes, edges };
-};
 
 const nodeTypes = {
   slide: Slide,
 };
 
+const initialSlide = '01';
+const { nodes, edges } = slidesToElements(initialSlide, slides);
+
 export default function XyflowDemo04() {
+  const [currentSlide, setCurrentSlide] = useState(initialSlide);
   const { fitView } = useReactFlow();
-  const { start, nodes, edges } = useMemo(() => slidesToElements(), []);
+
+  const handleKeyPress = useCallback<KeyboardEventHandler>(
+    (event) => {
+      const slide = slides[currentSlide];
+
+      switch (event.key) {
+        case 'ArrowLeft':
+        case 'ArrowUp':
+        case 'ArrowDown':
+        case 'ArrowRight': {
+          const direction = event.key.slice(5).toLowerCase() as keyof SlideData;
+          const target = slide[direction];
+
+          // Prevent the arrow keys from scrolling the page when React Flow is
+          // only part of a larger application.
+          event.preventDefault();
+
+          if (target) {
+            setCurrentSlide(target);
+            fitView({ nodes: [{ id: target }], duration: 100 });
+          }
+        }
+      }
+    },
+    [fitView, currentSlide],
+  );
 
   const handleNodeClick = useCallback<NodeMouseHandler>(
     (_, node) => {
-      fitView({ nodes: [{ id: node.id }], duration: 150 });
+      if (node.id !== currentSlide) {
+        setCurrentSlide(node.id);
+        fitView({ nodes: [{ id: node.id }], duration: 100 });
+      }
     },
-    [fitView],
+    [fitView, currentSlide],
   );
 
   return (
@@ -133,8 +74,9 @@ export default function XyflowDemo04() {
             nodeTypes={nodeTypes}
             edges={edges}
             fitView
-            fitViewOptions={{ nodes: [{ id: start }] }}
+            fitViewOptions={{ nodes: [{ id: initialSlide }], duration: 100 }}
             minZoom={0.1}
+            onKeyDown={handleKeyPress}
             onNodeClick={handleNodeClick}
           >
             <Controls />
